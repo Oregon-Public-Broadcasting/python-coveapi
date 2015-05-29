8d09de52aa22f2a924345b7e9e9ed6cc76723c96
Rob Vincent <rvincent@opb.org>
2015-05-29 13:30:17 -0700

    import COVEAPI_HOST from connection.py

--- coveapi/__init__.py
+++ coveapi/__init__.py
@@ -1,13 +1,10 @@
 """Package: `coveapi`
 A Python client for the PBS COVE API service.
 """
+from coveapi.connection import COVEAPIConnection, COVEAPI_HOST
 
 # client version
 __version__ = '0.2dev'
-
-# coveapi constants
-
-
 
 def connect(api_app_id, api_app_secret, api_host=COVEAPI_HOST):
     """Connect to the COVE API service.
@@ -20,5 +17,5 @@ def connect(api_app_id, api_app_secret, api_host=COVEAPI_HOST):
     Returns:
     `coveapi.connection.COVEAPIConnection` object
     """
-    from coveapi.connection import COVEAPIConnection
+    
     return COVEAPIConnection(api_app_id, api_app_secret, api_host)
